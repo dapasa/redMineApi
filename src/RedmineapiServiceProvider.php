041ef21907ba75c327bff74edcd114195b46ca8f
Daniel <dpassarelli@gmail.com>
2020-04-16 13:23:28 -0300

    .

---
+++ src/RedmineapiServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace dapasa\redMineApi;
+
+use Illuminate\Support\ServiceProvider;
+
+class RedmineapiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
